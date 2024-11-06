f54f7c553393796937cec29875c4ae2426088c04
a.turlyakov <a.turlyakov@dppm.pro>
2024-11-06 15:14:40 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <div class="div">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti enim explicabo facere fugit
+      pariatur quos repudiandae veritatis voluptatibus! Autem cum dolorem facere mollitia officia
+      repellat similique ut. Doloribus, fugit suscipit!
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts"></script>
